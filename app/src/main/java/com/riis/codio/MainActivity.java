a0eb4f6d1d3810d5c40fc83d83b4f517ceae0a07
Godfrey Nolan <godfrey@riis.com>
2015-11-19 16:30:21 -0500

    Update MainActivity.java

--- app/src/main/java/com/riis/codio/MainActivity.java
+++ app/src/main/java/com/riis/codio/MainActivity.java
@@ -1,9 +1,9 @@
 package com.riis.codio;
 
-import android.support.v7.app.AppCompatActivity;
+// import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
